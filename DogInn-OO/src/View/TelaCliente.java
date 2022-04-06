78af2f3e92fe0905388b0d7e122972808d9e8f44
gabri <gabri@LAPTOP-6CRIQP9J>
2022-04-06 09:53:14 -0300

    atualização código

--- DogInn-OO/src/View/TelaCliente.java
+++ DogInn-OO/src/View/TelaCliente.java
@@ -27,6 +27,7 @@ public class TelaCliente implements ActionListener {
 		janela.setSize(400, 250);
         janela.setVisible(true);
         
+        
         login.addActionListener(this);
         cadastro.addActionListener(this);
         
